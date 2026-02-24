bdfc69eb26badca06a19806ce425303e9fbd214c
Andrey Baidenko <andrey.lw@gmail.com>
2026-02-24 21:32:18 +0500

    fix: безопасное закрытие SettingsWindow без утечек подписок

--- JSQ/JSQ.UI.WPF/Views/SettingsWindow.xaml.cs
+++ JSQ/JSQ.UI.WPF/Views/SettingsWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows;
 using JSQ.UI.WPF.ViewModels;
 
@@ -5,12 +6,55 @@ namespace JSQ.UI.WPF.Views;
 
 public partial class SettingsWindow : Window
 {
+    private readonly SettingsViewModel _viewModel;
+
     public SettingsWindow(SettingsViewModel viewModel)
     {
         InitializeComponent();
+        _viewModel = viewModel;
         DataContext = viewModel;
 
-        viewModel.SaveCompleted += () => { DialogResult = true; };
-        viewModel.CancelRequested += () => { DialogResult = false; };
+        _viewModel.SaveCompleted += OnSaveCompleted;
+        _viewModel.CancelRequested += OnCancelRequested;
+
+        Closed += OnClosed;
+    }
+
+    private void OnSaveCompleted()
+    {
+        CloseWithDialogResult(true);
+    }
+
+    private void OnCancelRequested()
+    {
+        CloseWithDialogResult(false);
+    }
+
+    private void CloseWithDialogResult(bool result)
+    {
+        if (!Dispatcher.CheckAccess())
+        {
+            Dispatcher.Invoke(() => CloseWithDialogResult(result));
+            return;
+        }
+
+        if (!IsLoaded)
+            return;
+
+        try
+        {
+            DialogResult = result;
+        }
+        catch (InvalidOperationException)
+        {
+            Close();
+        }
+    }
+
+    private void OnClosed(object? sender, System.EventArgs e)
+    {
+        _viewModel.SaveCompleted -= OnSaveCompleted;
+        _viewModel.CancelRequested -= OnCancelRequested;
+        Closed -= OnClosed;
     }
 }
